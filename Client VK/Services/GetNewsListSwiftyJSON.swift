2d358bc32c1ae26e5538231b373a45804dce71f4
Vasily Petuhov <vspetukhov@icloud.com>
2020-09-28 15:48:10 +0300

    readme

--- Client VK/Services/GetNewsListSwiftyJSON.swift
+++ Client VK/Services/GetNewsListSwiftyJSON.swift
@@ -30,8 +30,8 @@ final class GetNewsListSwiftyJSON {
             urlConstructor.queryItems = [
                 URLQueryItem(name: "owner_id", value: String(Session.instance.userId)),
                 URLQueryItem(name: "access_token", value: Session.instance.token),
-//                URLQueryItem(name: "filters", value: "post,photo"),
-                URLQueryItem(name: "filters", value: "photo,wall_photo"),
+                URLQueryItem(name: "filters", value: "post,photo, wall_photo"),
+//                URLQueryItem(name: "filters", value: "photo,wall_photo"),
                 URLQueryItem(name: "start_from", value: nextNewsID),
 //                URLQueryItem(name: "count", value: "10"),
                 URLQueryItem(name: "v", value: "5.124")
